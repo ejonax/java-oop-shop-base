fc4f17f1d85eddda34662f87331ba1d278c6fbfe
ejonax <ejonax@yahoo.it>
2025-01-30 19:29:15 +0100

    update construction

--- src/Prodotto.java
+++ src/Prodotto.java
@@ -2,26 +2,41 @@ public class Prodotto {
    public int codice;
    public String nome;
    public String descrizione;
-   public int prezzo;
-   public int iva;
+   public float prezzo;
+   public float iva;
 
-    public Prodotto() {
-     codice = (int)(Math.random() * 101);
+    public Prodotto(String nome, String descrizione, float prezzo, float iva) {
+        this.nome=nome;
+        this.descrizione=descrizione;
+        this.prezzo=prezzo;
+        this.iva=iva;
     }
 
-    public void prezzoBase(int prezzoInput){
-       prezzo = prezzoInput;
-   }
-  
-   public void nome(String nome){
-    this.nome=nome;
-}
+    public int setCodice(){
+       return codice = (int)(Math.random() * 101);
+    }
 
-   public int prezzoCompresivo(int prezzoInput, int ivaInput){
-     return prezzo=prezzoInput + ivaInput;
-   }
+    public float getPrezzoBase(){
+        return this.prezzo;
+    }
 
-   public String nomeEsteso(){
-    return nome=" "+ codice + "-" + nome;
-   }
+    public String getProdottoName(){
+        return this.nome;
+    }
+
+    public String getProdottoDescription(){
+        return this.descrizione;
+    }
+
+    public float getIvaBase(){
+      return this.iva;
+    }
+
+    public float prezzoComprensivo(float prezzoInput, float ivaInput){
+       return prezzo=prezzoInput + (prezzoInput * (ivaInput/100));
+    }
+
+    public String nomeEsteso(){
+      return nome=" "+ codice + "-" + nome;
+    }
 }
--- src/usaProdotto.java
+++ src/usaProdotto.java
@@ -1,22 +1,34 @@
 public class usaProdotto {
      public static void main(String[] args) {
 
-         Prodotto shampoo=new Prodotto();
-         System.out.println("Il codice random dato al prodotto è "+ shampoo.codice);
+        /******* start prodotto shampoo *******/
+        Prodotto prod1=new Prodotto("shampoo", "cappelli secchi", 5 ,22);
 
-         shampoo.prezzo=5;
-         System.out.println("Il prezzo base dato è "+shampoo.prezzo);
+        prod1.codice=prod1.setCodice();
+        System.out.println("Il codice random dato al prodotto " +prod1.getProdottoName() + " è "+ prod1.codice);
 
-         shampoo.nome="shampoo";
-         System.out.println("Il nome dato è "+shampoo.nome);
 
-         shampoo.iva=3;
-         System.out.println("L'iva data è "+shampoo.iva);
+        System.out.println("Il prezzo base del prodotto è " + prod1.getPrezzoBase());
+        System.out.println("L'iva base del prodotto è " + prod1.getIvaBase());
 
-         //shampoo.prezzoCompresivo(shampoo.prezzo, shampoo.iva);
-         System.out.println("Il prezzo compresivo è " + (shampoo.prezzoCompresivo(shampoo.prezzo,shampoo.iva)));
+        System.out.println("Il prezzo comprensivo del prodotto è " + (prod1.prezzoComprensivo(prod1.prezzo,prod1.iva)));
 
-         System.out.println("Il nome esteso è " + (shampoo.nomeEsteso()));
+        System.out.println("Il nome esteso del prodotto è " + (prod1.nomeEsteso()) + " e ha come descrizione '" + prod1.getProdottoDescription() + "'");
+        /******* end prodotto shampoo *******/
 
+        /******* start prodotto libro *******/
+        Prodotto prod2=new Prodotto("libro","scenza",25,22 );
+
+        prod2.codice=prod2.setCodice();
+        System.out.println("Il codice random dato al prodotto " +prod2.getProdottoName() + " è "+ prod2.codice);
+ 
+ 
+        System.out.println("Il prezzo base del prodotto è " + prod2.getPrezzoBase());
+        System.out.println("L'iva base del prodotto è " + prod2.getIvaBase());
+ 
+        System.out.println("Il prezzo comprensivo del prodotto è " + (prod2.prezzoComprensivo(prod2.prezzo,prod2.iva)));
+ 
+        System.out.println("Il nome esteso del prodotto è " + (prod2.nomeEsteso()) + " e ha come descrizione '" + prod2.getProdottoDescription() + "'");
+        /******* end prodotto libro *******/
      }
 }
